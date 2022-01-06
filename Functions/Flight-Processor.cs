9d3ac71722cfa40b33a673a7eda4243b741dcb64
Adam Hems <adam@cloudydemos.com>
2022-01-06 06:46:00 -0600

    Added Application Insights Telemetry

--- Functions/Flight-Processor.cs
+++ Functions/Flight-Processor.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Cosmos;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace CloudyDemos.Aircraft
 {
@@ -27,14 +30,23 @@ namespace CloudyDemos.Aircraft
         public Location location { get; set; }
     }
 
-    public static class FlightProcessor
+    public class FlightProcessor
     {
         private static readonly string _databaseId = "Aircraft";
         private static readonly string _containerId = "flights";
         private static CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("cloudyaircraftdata_DOCUMENTDB"));
 
+        private readonly TelemetryClient telemetryClient;
+
+        /// Using dependency injection will guarantee that you use the same configuration for telemetry collected automatically and manually.
+        public FlightProcessor(TelemetryConfiguration telemetryConfiguration)
+        {
+            this.telemetryClient = new TelemetryClient(telemetryConfiguration);
+        }
+
+
         [FunctionName("FlightProcessor")]
-        public static void Run([CosmosDBTrigger(
+        public void Run([CosmosDBTrigger(
             databaseName: "Aircraft",
             collectionName: "flights",
             ConnectionStringSetting = "cloudyaircraftdata_DOCUMENTDB",
@@ -47,32 +59,37 @@ namespace CloudyDemos.Aircraft
                 try {
                     foreach(Document doc in input) {
 
-                    Flight flight = JsonConvert.DeserializeObject<Flight>(doc.ToString());
+                        Flight flight = JsonConvert.DeserializeObject<Flight>(doc.ToString());
 
-                    if(flight.location != null || flight.lat is null || flight.lon is null) 
-                        continue;
+                        if(flight.location != null || flight.lat is null || flight.lon is null) 
+                            continue;
 
-                    var db = cosmosClient.GetDatabase(_databaseId);
-                    var container = db.GetContainer(_containerId);
-                    
-                    // Convert lat & lon to a GeoSpatial Point so we can do distance calcs on it (https://docs.microsoft.com/en-us/azure/cosmos-db/sql/sql-query-geospatial-query)
-                    List<PatchOperation> patchOperations = new List<PatchOperation>();
-                    Location location = new Location { type = "Point", coordinates = new List<double>() { flight.lon.GetValueOrDefault(), flight.lat.GetValueOrDefault() } };
-                    patchOperations.Add(PatchOperation.Add("/Location", location));
-                    patchOperations.Add(PatchOperation.Remove("/lat"));
-                    patchOperations.Add(PatchOperation.Remove("/lon"));
-                    
-                    // Make Timestamp numeric
-                    if(!decimal.TryParse(flight.Timestamp, out var Timestamp)) {
-                        flight.Timestamp = ((DateTimeOffset)DateTime.Parse(flight.Timestamp)).ToUnixTimeSeconds().ToString();
+                        var db = cosmosClient.GetDatabase(_databaseId);
+                        var container = db.GetContainer(_containerId);
+                        
+                        // Convert lat & lon to a GeoSpatial Point so we can do distance calcs on it (https://docs.microsoft.com/en-us/azure/cosmos-db/sql/sql-query-geospatial-query)
+                        List<PatchOperation> patchOperations = new List<PatchOperation>();
+                        Location location = new Location { type = "Point", coordinates = new List<double>() { flight.lon.GetValueOrDefault(), flight.lat.GetValueOrDefault() } };
+                        patchOperations.Add(PatchOperation.Add("/Location", location));
+                        patchOperations.Add(PatchOperation.Remove("/lat"));
+                        patchOperations.Add(PatchOperation.Remove("/lon"));
+                        
+                        // Make Timestamp numeric
+                        if(!decimal.TryParse(flight.Timestamp, out var Timestamp)) {
+                            flight.Timestamp = ((DateTimeOffset)DateTime.Parse(flight.Timestamp)).ToUnixTimeSeconds().ToString();
+                        }
+                        patchOperations.Add(PatchOperation.Set("/Timestamp", decimal.Parse(flight.Timestamp)));
+                        
+                        container.PatchItemAsync<Flight>(
+                            id: flight.id,
+                            partitionKey: new Microsoft.Azure.Cosmos.PartitionKey(flight.flight),
+                            patchOperations: patchOperations);
+                        
+                        // Count this flight
+                        this.telemetryClient.GetMetric(flight.id).TrackValue(1);
+
                     }
-                    patchOperations.Add(PatchOperation.Set("/Timestamp", decimal.Parse(flight.Timestamp)));
-                    
-                    container.PatchItemAsync<Flight>(
-                        id: flight.id,
-                        partitionKey: new Microsoft.Azure.Cosmos.PartitionKey(flight.flight),
-                        patchOperations: patchOperations);
-                    }
+
                 } catch(Exception ex) {
                     log.LogError(ex, ex.Message);
                 }       
